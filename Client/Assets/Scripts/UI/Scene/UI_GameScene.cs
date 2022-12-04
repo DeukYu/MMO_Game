7941d122010b09f8d884f43542f65e94f215840b
DeukYu <ldy8070@naver.com>
2022-12-05 00:29:54 +0900

    Inventory
    Inventory

---
+++ Client/Assets/Scripts/UI/Scene/UI_GameScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_GameScene : UI_Scene
+{
+    //public UI_Stat StatUI { get; private set; }
+    public UI_Inventory InvenUI { get; private set; }
+    public override void Init()
+    {
+        base.Init();
+
+        InvenUI = 
+    }
+}
